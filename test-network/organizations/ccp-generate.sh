fbaf86ea7c3a1e80cea83a983d97c08dd7f0f4e4
Tiansir-wg <15872397013@163.com>
2020-08-07 16:56:56 +0800

    修改了 ccp文件生成脚本和模板文件

--- test-network/organizations/ccp-generate.sh
+++ test-network/organizations/ccp-generate.sh
@@ -10,6 +10,11 @@ function json_ccp {
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${CAPORT}/$3/" \
+        -e "s/\${P1PORT}/$6/" \
+        -e "s/\${P2PORT}/$7/" \
+        -e "s/\${P3PORT}/$8/" \
+        -e "s/\${P4PORT}/$9/" \
+        -e "s/\${P5PORT}/${10}/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
         organizations/ccp-template.json
@@ -21,35 +26,40 @@ function yaml_ccp {
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${CAPORT}/$3/" \
+        -e "s/\${P1PORT}/$6/" \
+        -e "s/\${P2PORT}/$7/" \
+        -e "s/\${P3PORT}/$8/" \
+        -e "s/\${P4PORT}/$9/" \
+        -e "s/\${P5PORT}/${10}/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
         organizations/ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
 }
 
 ORG=1
-P0PORT=7051
+P0PORT=17051
+P1PORT=18051
+P2PORT=19051
+P3PORT=20051
+P4PORT=21051
+P5PORT=22051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $P1PORT $P2PORT $P3PORT $P4PORT $P5PORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $P1PORT $P2PORT $P3PORT $P4PORT $P5PORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
 
 ORG=2
-P0PORT=9051
+P0PORT=27051
+P1PORT=28051
+P2PORT=29051
+P3PORT=30051
+P4PORT=31051
+P5PORT=32051
 CAPORT=8054
 PEERPEM=organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.example.com/connection-org2.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.example.com/connection-org2.yaml
-
-
-# ORG=3
-# P0PORT=10051
-# CAPORT=10054
-# PEERPEM=organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
-# CAPEM=organizations/peerOrganizations/org3.example.com/ca/ca.org3.example.com-cert.pem
-
-# echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org3.example.com/connection-org3.json
-# echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org3.example.com/connection-org3.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $P1PORT $P2PORT $P3PORT $P4PORT $P5PORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.example.com/connection-org2.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $P1PORT $P2PORT $P3PORT $P4PORT $P5PORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org2.example.com/connection-org2.yaml
